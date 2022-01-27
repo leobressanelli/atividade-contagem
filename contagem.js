daab54eb06a6a25c16715e43dc4742b24c59997e
Leonardo Ramos <leo.ramos1712@gmail.com>
2022-01-27 16:23:11 -0300

    Sistema de contagem de alunos finalizado

---
+++ contagem.js
@@ -0,0 +1,18 @@
+let numeroAlunos =10;
+
+for(let contador=0; contador < numeroAlunos; contador++){
+
+if(contador== 0){
+    
+    console.log("O número é ZERO")
+}
+    else if(contador % 2 == 0){
+
+        console.log(`O numero ${contador} é PAR`)
+
+    }else if(contador % 2 == 1){
+
+        console.log(`O numero ${contador} é IMPAR`)
+    }
+}
+
